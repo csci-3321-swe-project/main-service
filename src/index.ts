62bac855664dc55c969f92ad9ae97080993b6397
Forrest Keller <forrestblackburnkeller@gmail.com>
2023-03-05 14:31:49 -0600

    Implement routing stubs

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import dotenv from "dotenv";
 import express from "express";
+import courses from "./routes/courses";
+import users from "./routes/users";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
-app.get("/", (_, res) => {
-  res.send("Working.");
-});
+app.use("/courses", courses);
+app.use("/users", users);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
