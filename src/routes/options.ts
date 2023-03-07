6b064d9db7e58635b4ae613338d3827860e10a7b
Forrest Keller <forrestblackburnkeller@gmail.com>
2023-03-07 16:44:54 -0600

    Consolidate option routes

--- src/routes/options.ts
+++ src/routes/options.ts
@@ -4,20 +4,13 @@ import options from "../utilities/options";
 
 const router = Router();
 
-router.get("/roles", (req, res, next) => {
-  res.send(options(Role));
-});
-
-router.get("/departments", (req, res, next) => {
-  res.send(options(Department));
-});
-
-router.get("/days-of-week", (req, res, next) => {
-  res.send(options(DayOfWeek));
-});
-
-router.get("/terms", (req, res, next) => {
-  res.send(options(Term));
+router.get("/", (_, res) => {
+  res.send({
+    roles: options(Role),
+    departments: options(Department),
+    daysOfWeek: options(DayOfWeek),
+    terms: options(Term),
+  });
 });
 
 export default router;
