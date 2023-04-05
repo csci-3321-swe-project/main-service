935d66f91b23e8b8e3f9c0c3383e04d2d546d64f
Forrest Keller <forrestblackburnkeller@gmail.com>
2023-04-04 19:55:31 -0500

    Improve documentation

--- src/utilities/query-param.ts
+++ src/utilities/query-param.ts
@@ -2,7 +2,7 @@ import { z, ZodSchema } from "zod";
 
 /**
  * Creates a zod schema that accepts either a single value or an array of values
- * @param t zod schema
+ * @param {ZodSchema} t zod schema
  * @returns zod schema that accepts either a single value or an array of values
  */
 const queryArrayParam = (t: ZodSchema) => {
