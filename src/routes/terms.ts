daf096212297a97a270b6a3916d6994cc7f559ce
clee9 <78444840+clee9@users.noreply.github.com>
2023-05-03 09:28:03 -0500

    try

--- src/routes/terms.ts
+++ src/routes/terms.ts
@@ -374,10 +374,10 @@ router.delete(
       });
 
       res.status(200).send(deletedTerm);
-      res.status(200).json({ message: "Term deleted successfully" });
+      res.status(200).send({ message: "Term deleted successfully" });
     } catch (err) {
       console.log(err);
-      res.status(500).json({ message: "Internal server error" });
+      res.status(500).send({ message: "Internal server error" });
       next(err);
     }
   }
